91449b95c1219169c18e07ed43e45569d3c39bc7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-08 17:21:01 +0100

    code cleanup

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -27,23 +27,6 @@ async function page({ searchParams }: Props) {
                 </>
             )}
         </div>
-        {/* {products.length ? (
-            <div className='flex flex-col items-center min-h-screen bg-gray-100 p-4'>
-                <div className='bg-white p-8 rounded-lg shadow-md w-full max-w-4xl'>
-                    <h1 className='text-3xl font-bold mb-6 text-center'>Search results for {query}</h1>
-                    <ProductGrid products={products} />
-                </div>
-            </div>
-        ) : (
-            <div className='flex flex-col items-center min-h-screen bg-gray-100 p-4'>
-                <div className='bg-white p-8 rounded-lg shadow-md w-full max-w-4xl'>
-                    <h1 className='text-3xl font-bold mb-6 text-center'>No products found for {query}</h1>
-                    <p className='text-gray-600 text-center'>
-                        Try searching with different keywords
-                    </p>
-                </div>
-            </div>
-        )} */}
     </div>
   )
 }
